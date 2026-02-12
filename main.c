79e90d581fbb9c3d8b653485ee78083e748a13d6
Lumi <lumilebest@gmail.com>
2026-02-12 17:47:58 +0100

    je modif le main

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <raylib.h>
+#include "raylib.h"
 
-int main(){
-    
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void)
+{
+    InitWindow(800, 450, "Test Raylib");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Ca marche !", 300, 200, 40, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
